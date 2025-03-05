0585a9131ad9c01763e4cbf3efe0f727e33f298b
Konstantin Pankratov <Konstantin.Pankratov@adorsys.com>
2025-03-05 12:40:38 +0100

    init?

---
+++ src/main/java/com/example/demo/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Autowired
+  JwtAuthConverter jwtAuthConverter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.
+        authorizeHttpRequests(a -> a
+            .requestMatchers("/hello").hasRole("USER")
+            .requestMatchers("/helloAdmin").hasRole("ADMIN")
+            .anyRequest().authenticated())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+    return http.build();
+  }
+}
